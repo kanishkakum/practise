3be5e5f6c8f16062e900bcff5419645b16ca6138
kanishkakumar408 <kanishka.kumar@berylsystem.com>
2020-12-17 11:35:53 +0530

    added change column with boolean

--- BookShop/db/migrate/20201217060345_add_boolean_to_newcolumn.rb
+++ BookShop/db/migrate/20201217060345_add_boolean_to_newcolumn.rb
@@ -1,5 +1,5 @@
 class AddBooleanToNewcolumn < ActiveRecord::Migration[6.0]
   def change
-  	add_column :products, :delete, :boolean, :null=>true
+  	change_column :products, :delete, :boolean, :null=>true, :default=>false
   end
 end
