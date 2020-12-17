66a04c187c3edc66bce1caa8fa559b87d95f98cc
kanishkakumar408 <kanishka.kumar@berylsystem.com>
2020-12-17 13:47:28 +0530

    added route of undelete action

--- BookShop/config/routes.rb
+++ BookShop/config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   resources :products 
+  root 'products#index'
   post "destroy_multiple", to: "products#destroy_multiple", as: "products_destroy"
   get "/soft-delete/:id" => "products#soft_delete"
+  get "/un-delete/:id" => "products#undeleted"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
