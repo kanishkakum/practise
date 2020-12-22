31d711ad31a38259f727c7c1dbd6fc2a511d0330
kanishkakumar408 <kanishka.kumar@berylsystem.com>
2020-12-22 15:06:20 +0530

    created create ,edit , update ,destroy action

--- store/app/controllers/products_controller.rb
+++ store/app/controllers/products_controller.rb
@@ -1,7 +1,56 @@
 class ProductsController < ApplicationController
+	before_action :set_product, only: [:show, :edit, :update, :destroy, :update]
   def index
+  	@products = Product.all
+  end
+
+  def create
+  	@product = Product.new(product_params)
+
+    respond_to do |format|
+      if @product.save
+        format.html { redirect_to products_path, notice: 'Product was successfully created.' }
+    
+      else
+        format.html { render :new }
+        
+      end
+    end
+  end
+  def update
+    respond_to do |format|
+      if @product.update(product_params)
+        format.html { redirect_to products_path, notice: 'Product was successfully updated.' }
+      else
+        format.html { render :edit }
+ 
+      end
+    end
+  end
+
+  def destroy
+    @product.destroy
+    respond_to do |format|
+      format.html { redirect_to products_url, notice: 'Product was successfully deleted.' }
+    end
+  end
+
+  def new
+  	@product = Product.new
   end
 
   def show
+  	
   end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_product
+      @product = Product.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def product_params
+      params.require(:product).permit(:Title, :description, :price, :image_url, :color)
+    end
 end
--- store/config/routes.rb
+++ store/config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   root 'products#index'	
   
-  get 'products', to: 'products#index', as: 'products'   
-  get 'products/:id', to: 'products#show', as: 'product'
+  resources "products"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
