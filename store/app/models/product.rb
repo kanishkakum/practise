5e9a8e7570540b06920a2347f8ec7177387b269c
kanishkakumar408 <kanishka.kumar@berylsystem.com>
2020-12-24 15:45:20 +0530

    Created a custom validation to limit the letter lenghts of title

--- store/app/models/product.rb
+++ store/app/models/product.rb
@@ -1,8 +1,21 @@
 class Product < ApplicationRecord
   validates :title , :price, :image_url, :description, presence: true
   validates :price , numericality: true
-  validates :title , uniqueness: true , format: { :with => /\A[a-zA-Z]+\z/ }
+  validates :title , uniqueness: true , format: { with:  /\A[a-zA-Z]+\z/ }
   validates :image_url, allow_blank: true, format: { with: %r{\.(gif|jpg|png)\z}i }
+
+  validate :title_limits
+
+  private
+  def title_limits
+     if title.length > 20
+      errors.add(:title, "cannot be above 20 letters")
+    elsif title.length < 5
+      errors.add(:title, "cannot be below 5 letters")
+    end
+  end  
+
+
 
   def self.search(search)
 	if search
