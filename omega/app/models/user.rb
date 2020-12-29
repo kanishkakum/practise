495691c208e335924588022a9538641e2b87aa81
kanishkakumar408 <kanishka.kumar@berylsystem.com>
2020-12-29 19:51:10 +0530

    added validation to email, username, password

--- omega/app/models/user.rb
+++ omega/app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :username, :email, presence: true
+  validates :username, :email, uniqueness: true
+  validates :password, length: { minimum: 8 }, unless: "password.nil?"
+  validates :password, presence: true, if: "id.nil?"
+  validates :username, presence:true, uniqueness: true
+
 end
