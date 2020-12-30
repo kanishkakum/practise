ea6ee7ef32b2bc3b6a063862e2a07317943f784f
kanishkakumar408 <kanishka.kumar@berylsystem.com>
2020-12-30 12:39:07 +0530

    permitted date of birth column when user try to sign up

--- omega/app/controllers/application_controller.rb
+++ omega/app/controllers/application_controller.rb
@@ -6,8 +6,9 @@ class ApplicationController < ActionController::Base
   protected
   
     def configure_permitted_parameters
-  	  devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :email, :location, :password, :password_confirmation])
+  	  devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :email, :Date_of_birth, :location, :password, :password_confirmation])
   	  devise_parameter_sanitizer.permit(:account_update, keys: [:username, :email, :location, :password, :password_confirmation])
+  	  devise_parameter_sanitizer.permit(:sign_in, keys: [:username, :email, :password])
     end
   
 end
