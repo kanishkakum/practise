5d2f69b85e7607add3ba2ac7a9694361e994f598
kanishkakumar408 <kanishka.kumar@berylsystem.com>
2020-12-30 13:39:49 +0530

    added username as an authentication key for login

--- omega/app/models/user.rb
+++ omega/app/models/user.rb
@@ -2,11 +2,13 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, authentication_keys: [:email, :username]
 
   validates :username, :email, presence: true
   validates :username, :email, uniqueness: true
  
   validates :username, presence:true, uniqueness: true
+
+ 
 
 end
