2a8c2124ea51fbeaa82776d6812da4086c3abc71
kanishkakumar408 <kanishka.kumar@berylsystem.com>
2020-12-31 19:10:40 +0530

    added update action in controller

--- Apex/app/controllers/api/v1/products_controller.rb
+++ Apex/app/controllers/api/v1/products_controller.rb
@@ -24,9 +24,16 @@ module Api
         product = Product.find(params[:id])
         product.destroy
         render json: {status: 'ERROR', message:'Deleted product', data:product}, status: :ok  
+      end 
 
-
-      end  
+      def create
+        product = Product.find(params[:id])
+        if product.update_attributes(product_params)
+          render json: {status: 'SUCCESS', message:'product updated successfully', data:product}, status: :ok 
+        else
+          render json: {status: 'ERROR', message:'product not updated', data:product.errors}, status: :unprocessable_entity   
+        end  
+      end 
 
       private
 
