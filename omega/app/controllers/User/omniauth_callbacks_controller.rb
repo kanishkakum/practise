84830dc4fd17090148ec1e1d61507799d228ce83
kanishkakumar408 <kanishka.kumar@berylsystem.com>
2020-12-31 14:05:45 +0530

    added google_oauth method for omniauth callback

--- omega/app/controllers/User/omniauth_callbacks_controller.rb
+++ omega/app/controllers/User/omniauth_callbacks_controller.rb
@@ -1,23 +1,23 @@
-# frozen_string_literal: true
-
 class User::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def google_oauth2
-    admin = Admin.from_google(from_google_params)
 
-    if admin.present?
+    from_google_params
+    user = create_user
+
+    if user.present?
       sign_out_all_scopes
-      flash[:success] = t 'devise.omniauth_callbacks.success', kind: 'Google'
-      sign_in_and_redirect admin, event: :authentication
+      flash[:success] = t 'devise.omniauth_callbacks.success', kind: 'Google' 
+      sign_in_and_redirect user, event: :authentication
     else
       flash[:alert] = t 'devise.omniauth_callbacks.failure', kind: 'Google', reason: "#{auth.info.email} is not authorized."
-      redirect_to new_admin_session_path
+      redirect_to new_user_session_path
     end
   end
 
   protected
 
   def after_omniauth_failure_path_for(_scope)
-    new_admin_session_path
+    new_user_session_path
   end
 
   def after_sign_in_path_for(resource_or_scope)
@@ -28,14 +28,17 @@ class User::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
   def from_google_params
     @from_google_params ||= {
-      uid: auth.uid,
       email: auth.info.email,
-      full_name: auth.info.name,
-      avatar_url: auth.info.image
+      username: auth.info.username
+      
     }
   end
 
   def auth
     @auth ||= request.env['omniauth.auth']
+  end
+
+  def create_user
+    User.create_with(skip_password_validation: true, username: @from_google_params[:username]).find_or_create_by(email: @from_google_params[:email])
   end
 end
