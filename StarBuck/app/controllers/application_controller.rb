73edfc8f59f14e6f00a5251b25082ab262e5c259
kanishkakumar408 <kanishka.kumar@berylsystem.com>
2021-01-04 23:57:05 +0530

    added code which process a jwt when a user sends a request to our api

--- StarBuck/app/controllers/application_controller.rb
+++ StarBuck/app/controllers/application_controller.rb
@@ -1,2 +1,33 @@
 class ApplicationController < ActionController::API
+  respond_to :json
+  before_action :process_token
+
+  private
+
+  def process_token
+  	if request.headers['Authorization'].presents?
+  	  begin
+  	  jwt_payload =  JWT.decode(request.headers['Authorization'], 's3cr3t', true, algorith: 'HS256')
+  	  @current_user_id = jwt_payload[0]['user_id'].to_i
+  	    puts jwt_payload
+      rescue JWT::ExpiredSignature, JWT::VerificationError, JWT::DecodeError
+        head :unauthorized
+      end
+    end
+  end
+  
+
+  def authenticate_user!(options = {})
+    head :unauthorized unless signed_in?
+  end
+
+  def signed_in?
+    @current_user_id.present?
+  end
+
+
+  def current_user
+    @current_user ||= super || User.find(@current_user_id)
+  end	
 end
+
