fc11582a9102e086b5f2bac2d405c08b4e68163f
kanishkakumar408 <kanishka.kumar@berylsystem.com>
2021-01-05 17:47:45 +0530

    created application controller for token authentication

--- StarBuck/app/controllers/application_controller.rb
+++ StarBuck/app/controllers/application_controller.rb
@@ -2,20 +2,19 @@ class ApplicationController < ActionController::API
   respond_to :json
   before_action :process_token
 
-  private
+  private 
 
   def process_token
-  	if request.headers['Authorization'].presents?
-  	  begin
-  	  jwt_payload =  JWT.decode(request.headers['Authorization'], 's3cr3t', true, algorith: 'HS256')
-  	  @current_user_id = jwt_payload[0]['user_id'].to_i
-  	    puts jwt_payload
+    if request.headers['Authorization'].present?
+      begin
+        jwt_payload = JWT.decode(request.headers['Authorization'], 's3cr3t', true, algorithm: 'HS256')
+        @current_user_id = jwt_payload[0]['user_id'].to_i
+          puts jwt_payload
       rescue JWT::ExpiredSignature, JWT::VerificationError, JWT::DecodeError
         head :unauthorized
       end
     end
   end
-  
 
   def authenticate_user!(options = {})
     head :unauthorized unless signed_in?
@@ -25,9 +24,8 @@ class ApplicationController < ActionController::API
     @current_user_id.present?
   end
 
-
   def current_user
-    @current_user ||= super || User.find(@current_user_id)
-  end	
-end
+    @current_user ||= super || User.find_by(id: @current_user_id)
+  end 
 
+end
