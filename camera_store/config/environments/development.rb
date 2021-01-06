5d9c210bb9d67f9caace2fd40c3fafc45cae55be
kanishkakumar408 <kanishka.kumar@berylsystem.com>
2021-01-06 14:12:47 +0530

    database schema

--- camera_store/config/environments/development.rb
+++ camera_store/config/environments/development.rb
@@ -50,5 +50,8 @@ Rails.application.configure do
 
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
+  config.hosts << "2b64063a10f5.ngrok.io"
+
+
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 end
