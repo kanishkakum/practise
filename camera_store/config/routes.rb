432fe0d50abee3c3b79764ce8ffe06daec4735aa
kanishkakumar408 <kanishka.kumar@berylsystem.com>
2021-01-06 14:10:13 +0530

    added route

--- camera_store/config/routes.rb
+++ camera_store/config/routes.rb
@@ -1,4 +1,17 @@
 Rails.application.routes.draw do
-  devise_for :users
+   namespace 'api' do
+    namespace 'v1' do
+      devise_for :users,
+      controllers: {
+        registrations: 'api/v1/registrations',
+         sessions: 'api/v1/sessions'
+      }
+
+      devise_scope :user do
+        get "/" => "home#index"
+      end
+    end
+  end  	
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
