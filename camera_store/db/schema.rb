b9fefb056cde5a1feff43e8845916d8a2ae0f16e
kanishkakumar408 <kanishka.kumar@berylsystem.com>
2021-01-06 16:48:15 +0530

    added migrationin product for reference to user, added asscoiation

--- camera_store/db/schema.rb
+++ camera_store/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_06_093004) do
+ActiveRecord::Schema.define(version: 2021_01_06_102435) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,7 +31,9 @@ ActiveRecord::Schema.define(version: 2021_01_06_093004) do
     t.integer "make"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id", null: false
     t.index ["category_id"], name: "index_products_on_category_id"
+    t.index ["user_id"], name: "index_products_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -47,4 +49,5 @@ ActiveRecord::Schema.define(version: 2021_01_06_093004) do
   end
 
   add_foreign_key "products", "categories"
+  add_foreign_key "products", "users"
 end
