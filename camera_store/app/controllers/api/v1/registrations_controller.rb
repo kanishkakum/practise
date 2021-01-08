66db19065a7412df7ada3e675388df990d206ddc
kanishkakumar408 <kanishka.kumar@berylsystem.com>
2021-01-08 14:32:26 +0530

    created create cart when a user sign up

--- camera_store/app/controllers/api/v1/registrations_controller.rb
+++ camera_store/app/controllers/api/v1/registrations_controller.rb
@@ -5,17 +5,21 @@ class  Api::V1::RegistrationsController < Devise::RegistrationsController
     user = User.new(sign_up_params)
 
     if user.save
-    token = JWT.encode({user_id: User.find_by(email: sign_up_params['email']).id}, 's3cr3t')
-
-    puts token
-    puts user.id
-    cart = Cart.create[:user_id]
+      create_cart(user.id)
+      token = JWT.encode({user_id: User.find_by(email: sign_up_params['email']).id}, 's3cr3t')
+    
+      puts token
+      puts user.id
       render json: token.to_json
     else
       render json: { errors: { 'email or password' => ['is invalid'] } }, status: :unprocessable_entity
     end
   end
 
+
+  def create_cart(id)
+    @cart = Cart.create(user_id: id)
+  end
 
   private
   def sign_up_params
