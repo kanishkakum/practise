a708ff958f0863e9a237d93ec187dbd159da7a7d
kanishkakumar408 <kanishka.kumar@berylsystem.com>
2021-01-12 18:21:27 +0530

    before_save callback to increament price of product

--- camera_store/app/models/product.rb
+++ camera_store/app/models/product.rb
@@ -2,4 +2,16 @@ class Product < ApplicationRecord
   belongs_to :category
   #belongs_to :user
   has_and_belongs_to_many :carts
+
+  scope :min_price, -> () {where("price > 50000")}
+  scope :x, -> () {where(category_id: 2)}
+  scope :y, -> () {where(category_id: Category.where(name: 'nikon'))}
+  scope :b, -> () {where(id: [1,2,3,4])}
+  scope :price_less_than, -> (amount) {where("price > ?", amount)}
+
+  before_save :inc_price
+
+  def inc_price
+  	self.price = price*1.20
+  end	
 end
