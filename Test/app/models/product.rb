86f2ebf130bb9e7e125608437f84b9d816798799
kanishkakumar408 <kanishka.kumar@berylsystem.com>
2021-01-13 17:00:36 +0530

    applied custom validation to check the validate length of name

--- Test/app/models/product.rb
+++ Test/app/models/product.rb
@@ -1,3 +1,15 @@
 class Product < ApplicationRecord
-  belongs_to :user	
+  belongs_to :user
+  validates :name , uniqueness: true
+  validate :name_limit
+  
+  private
+
+   def name_limit
+    if name.length > 15
+      errors.add(:name, “cannot be above than 15”)
+    elsif name.length < 3
+      errors.add(:name, “cannot be below than 3”)
+    end
+  end
 end
