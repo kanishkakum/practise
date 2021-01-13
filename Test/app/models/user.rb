d48cdd287cb44e9bea9716a9a144490fcfa7e3ab
kanishkakumar408 <kanishka.kumar@berylsystem.com>
2021-01-13 15:42:05 +0530

    added email uniqueness validation, email format validation, numericality validation for salary

--- Test/app/models/user.rb
+++ Test/app/models/user.rb
@@ -2,5 +2,7 @@ class User < ApplicationRecord
   has_many :products	
   validates_associated :products
   validates :name, :adhar_no, presence: true
-  validates :email, format: { /[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]/ , message: "string , numeric and special symbols are allowed" }
+  validates :email, format: {with: /[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]/, message: "string , numeric and special symbols are allowed" }
+  validates :email, uniqueness: true
+  validates :salary, numericality: true
 end
