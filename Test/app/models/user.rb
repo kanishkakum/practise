8e025efa305952d7d5a75daa5544759ca1e324fc
kanishkakumar408 <kanishka.kumar@berylsystem.com>
2021-01-13 17:50:17 +0530

    created custom validation for salary limit

--- Test/app/models/user.rb
+++ Test/app/models/user.rb
@@ -2,7 +2,16 @@ class User < ApplicationRecord
   has_many :products	
   validates_associated :products
   validates :name, :adhar_no, presence: true
-  validates :email, format: {with: /[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]/, message: "string , numeric and special symbols are allowed" }
+  #validates :email, format: {with: /[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]/, message: "string , numeric and special symbols are allowed" }
   validates :email, uniqueness: true
   validates :salary, numericality: true
+  validate :salary_limit
+
+  private
+
+  def salary_limit
+  	if salary < 20000
+  	  errors.add(:salary, "He is unable to buy product")
+  	end
+  end	  	
 end
