d5bdfa24d048b7b3499a22e28d78329d79ab666e
kanishkakumar408 <kanishka.kumar@berylsystem.com>
2021-01-14 19:30:55 +0530

    created form using form_with, created view page for new action

--- Test/app/models/product.rb
+++ Test/app/models/product.rb
@@ -2,6 +2,7 @@ class Product < ApplicationRecord
   belongs_to :user
   validates :name , uniqueness: true
   validate :name_limit
+  validate :Remove_space_from_name
   
   private
 
@@ -11,5 +12,12 @@ class Product < ApplicationRecord
      elsif name.length < 3
        errors.add(:name, "cannot be below than 3")
      end
+   end
+
+   def Remove_space_from_name()
+     if name.include?(' ')
+       name.gsub(' ','')
+     end  
+         
    end
 end
