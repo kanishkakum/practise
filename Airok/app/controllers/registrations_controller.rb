fc2c6349aadaa27704b153bd06f9b8de2452d3bb
kanishkakumar408 <kanishka.kumar@berylsystem.com>
2021-01-16 21:52:51 +0530

    created registration controller for user creation

---
+++ Airok/app/controllers/registrations_controller.rb
@@ -0,0 +1,27 @@
+class RegistrationsController < Devise::RegistrationsController
+  before_action :permit_all_params
+  skip_before_action :verify_authenticity_token	
+
+
+  def create
+  	user = User.create(sign_up_params)
+  	if user.save
+  	  token = JWT.encode({user_id: User.find_by(email: sign_up_params['email'].id)}, 's3cr3t')
+  	  puts token
+  	  puts user.id
+  	  render json: token.to_json
+  	else
+  	  render json: {errors: {'email or password' => ['is invalid']}}, status: :unprocessable_entity
+  	end
+  end
+
+  def sign_up_params
+    params.except('controller', 'action')
+  end
+  
+  def permit_all_params
+    params.permit!
+  end    	    
+
+
+end	
--- Airok/app/models/user.rb
+++ Airok/app/models/user.rb
@@ -1,6 +1,13 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable	
   has_many :addresses
   has_one :cart	
   has_one :account
-  has_one :accounthistory , :through => :account
+  has_many :categories , :through => :products
+
 end
