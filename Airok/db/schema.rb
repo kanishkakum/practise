846871e3d8c6d61a41bab50d669fc12413fd7df6
kanishkakumar408 <kanishka.kumar@berylsystem.com>
2021-01-16 12:39:28 +0530

    created cartsProduct model, added associated in cart and product model

--- Airok/db/schema.rb
+++ Airok/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_16_065825) do
+ActiveRecord::Schema.define(version: 2021_01_16_070509) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,15 @@ ActiveRecord::Schema.define(version: 2021_01_16_065825) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_carts_on_user_id"
+  end
+
+  create_table "carts_products", force: :cascade do |t|
+    t.bigint "cart_id", null: false
+    t.bigint "product_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["cart_id"], name: "index_carts_products_on_cart_id"
+    t.index ["product_id"], name: "index_carts_products_on_product_id"
   end
 
   create_table "categories", force: :cascade do |t|
@@ -62,5 +71,7 @@ ActiveRecord::Schema.define(version: 2021_01_16_065825) do
 
   add_foreign_key "addresses", "users"
   add_foreign_key "carts", "users"
+  add_foreign_key "carts_products", "carts"
+  add_foreign_key "carts_products", "products"
   add_foreign_key "products", "categories"
 end
