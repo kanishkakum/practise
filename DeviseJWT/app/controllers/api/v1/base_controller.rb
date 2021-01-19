62e0e56778e3d5912c8b7cfd7b6f68442e4cfb5e
kanishkakumar408 <kanishka.kumar@berylsystem.com>
2021-01-19 16:40:37 +0530

    created base controller inherting from api to decode token for authorization

--- DeviseJWT/app/controllers/api/v1/base_controller.rb
+++ DeviseJWT/app/controllers/api/v1/base_controller.rb
@@ -1,4 +1,31 @@
 class BaseController < ActionController::API
+  respond_to :json
+  before_action :process_token
+  skip_before_action :verify_authenticity_token
 
+  private
 
+  def process_token
+  	if request.headers['Authorization'].present?
+  	  begin
+  	  	jwt_payload = JWT.decode(request.headers['Authorization'], 's3cr3t', true, algorithm: 'HS256')
+  	  	@curren_user_id = jwt.payload[0]['user_id'].to_i
+  	  	  puts jwt_payload
+      rescue JWT::ExpiredSignature, JWT::VerificationError, JWT::DecodeError
+      	render json: {status: 404, message: "unauthorized user"}
+      end
+    end
+  end
+  
+  def authenticate_user(option = {})
+    head :unauthorized unless signed_in?
+  end
+
+  def signed_in?
+  	@curren_user_id.present?
+  end
+  
+  def curren_user
+    @current_user ||=super || User.find_by(id: @curren_user_id)
+  end  	
 end	
