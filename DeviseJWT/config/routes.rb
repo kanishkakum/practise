960ac93399ef5ddd5065ebf0e05f727fb79c3f8f
kanishkakumar408 <kanishka.kumar@berylsystem.com>
2021-01-19 16:41:30 +0530

    added routes

--- DeviseJWT/config/routes.rb
+++ DeviseJWT/config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  namespace 'api' do
+  	namespace 'v1' do
+  	  devise_for :users,
+  	  controller: {
+  	  	registrations: 'api/v1/registrations'
+  	  	sessions: 'api/v1/sessions'
+  	  }
+  		
+  	end
+  end	
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
