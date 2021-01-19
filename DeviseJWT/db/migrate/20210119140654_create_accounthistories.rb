95d922f843f5f60737c0f27f09304444203a0ad4
kanishkakumar408 <kanishka.kumar@berylsystem.com>
2021-01-19 20:48:56 +0530

    created account and accounthsitorty model

---
+++ DeviseJWT/db/migrate/20210119140654_create_accounthistories.rb
@@ -0,0 +1,10 @@
+class CreateAccounthistories < ActiveRecord::Migration[6.0]
+  def change
+    create_table :accounthistories do |t|
+      t.references :account, null: false, foreign_key: true
+      t.decimal :credit
+
+      t.timestamps
+    end
+  end
+end
